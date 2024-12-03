27ce1622b565c8ffcb2e9ae43267a826d7c2809b
TheAliBit <the.ali8bits@gmail.com>
2024-12-03 05:33:50 -0800

    feat: add the player models

--- player/models.py
+++ player/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    parent = models.ForeignKey('self', related_name='children', on_delete=models.SET_NULL, null=True, blank=True)
+
+
+class Music(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='uploads/images/')
+    music = models.FileField(upload_to='uploads/music')
+
+    def __str__(self):
+        return self.title
